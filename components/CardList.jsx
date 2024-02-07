67cfd0bd2cb1906ae14a0f24c6cf22aa0ba898e7
Vadim <bezvadimka@mail.ru>
2024-02-07 19:49:50 +0300

    route card-list

--- components/CardList.jsx
+++ components/CardList.jsx
@@ -1,7 +1,12 @@
-const React = require('react');
+const React = require("react");
+const Card = require('./Card')
 
-module.exports = function CardList({ cards }) {
+module.exports = function CardList({ properties }) {
   return (
-    <div />
+    <div className="cardList">
+      {properties.map((el) => (
+        <Card key={el.id} property={el} />
+      ))}
+    </div>
   );
 };
