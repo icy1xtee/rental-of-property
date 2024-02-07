ab3563eebc3743e73694f45b9eb844225e39a6dd
icy1xtee <easton2904@mail.ru>
2024-02-07 11:47:43 +0300

    dev init

--- routes/view/auth.route.js
+++ routes/view/auth.route.js
@@ -2,10 +2,7 @@ const router = require('express').Router();
 const Layout = require('../../components/Layout');
 
 router.get('/', (req, res) => {
-  const html = res.renderComponent(Layout, {
 
-  });
-  res.status(201).send(html);
 });
 
 module.exports = router;
