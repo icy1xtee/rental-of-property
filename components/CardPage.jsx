1527b779e49a3660b18f436ac15823020ff60705
icy1xtee <easton2904@mail.ru>
2024-02-07 19:02:28 +0300

    cardPage conponent done

--- components/CardPage.jsx
+++ components/CardPage.jsx
@@ -1,10 +1,23 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-module.exports = function CardPage() {
+module.exports = function CardPage({ title, property }) {
   return (
-    <Layout>
-      <div />
+    <Layout title={title}>
+      <div className="images">
+        {property.Imgs.map((img) => <image className="images" src={img.url} />)}
+      </div>
+      <div className="information">
+        <p>{property.title}</p>
+        <p>{property.address}</p>
+        <p>{property.price}</p>
+      </div>
+      <div className="description">
+        <p>{property.description}</p>
+      </div>
+      <div className="map">
+        <p>{property.coordinates}</p>
+      </div>
     </Layout>
   );
 };
