298056e2dea1905d9d0e56ff985d9481d8681655
Vadim <bezvadimka@mail.ru>
2024-02-08 13:13:20 +0300

    404

---
+++ components/IncorrectPage.jsx
@@ -0,0 +1,17 @@
+const React = require("react");
+const Layout = require("./Layout");
+
+module.exports = function IncorrectPage() {
+  return (
+    <Layout>
+      <div className="incorrect">
+        <img
+          alt="..."
+          src="https://musor-vyvoz-spb.ru/assets/images/glav2-31.jpg"
+        />
+        <p>The page you are looking for not avaible!</p>
+        <a href="" class="link_404">Go to Home</a>
+      </div>
+    </Layout>
+  );
+};
