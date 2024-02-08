74c37d84d228a6eea7d8ab8ece53aa75df01dcd9
ZakharchenkoV <zvk096@gmail.com>
2024-02-08 13:49:24 +0300

    filter func init

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -1,15 +1,21 @@
-const React = require("react");
-const Layout = require("./Layout");
-const CardList = require('./CardList')
+const React = require('react');
+const Layout = require('./Layout');
+const CardList = require('./CardList');
 
 function MainPage({ title, categories, properties }) {
   return (
     <Layout title={title}>
       <div>
         <p>filter</p>
-        {categories.map((el) => (
-          <p>{el.name}</p>
-        ))}
+        <form>
+          {categories.map((el, index) => (
+            <div>
+              <input type="radio" id={el.id} name="category" value={el.name} />
+              <label>{el.name}</label>
+            </div>
+          ))}
+          <button type="submit">Submit</button>
+        </form>
       </div>
       <div>
         <CardList properties={properties} />
