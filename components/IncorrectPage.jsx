289deb5a59d944cccc67cd9022ac61a7a620d9fb
icy1xtee <easton2904@mail.ru>
2024-02-08 12:29:50 +0300

    404 page naming fixed

--- components/IncorrectPage.jsx
+++ components/IncorrectPage.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-module.exports = function ErrorPage() {
+module.exports = function IncorrectPage() {
   return (
     <Layout>
       <div />
