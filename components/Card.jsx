844205e4c747ffb92c1684169a90df67f62168ba
icy1xtee <easton2904@mail.ru>
2024-02-08 19:57:09 +0300

    admin functionality halfly done

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,13 +2,13 @@ const React = require('react');
 
 module.exports = function Card({ property, user }) {
   return (
-    <div className="cardList__containter-card">
+    <div className="cardList__containter-card" data-id={property.id}>
       <p className="card-favicon">isfavorite</p>
       <image className="card-picture" src={property.Imgs[0].url} alt="pic" />
       <p className="card-title">{property.title}</p>
       <p className="card-price">{property.price}</p>
       <button className="card-button" type="button" id={property.id}>Посмотреть</button>
-      {user.isAdmin && <button className="admin-button-delete" type="button">Удалить</button>}
+      {user.isAdmin && <button className="admin-button-delete" id={property.id} type="button">Удалить</button>}
     </div>
   );
 };
