ffa95f42565afee48532960ee89e7fd59f59e969
Vadim <bezvadimka@mail.ru>
2024-02-08 15:31:46 +0300

    style

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,6 +8,8 @@ module.exports = function Layout({ title, children }) {
         <meta charSet="UTF-8" />
         <link rel="stylesheet" href="/css/style-reset.css" />
         <link rel="stylesheet" href="/css/styles.css" />
+        <link rel="stylesheet" href="/css/navbar.css" />
+        <link rel="stylesheet" href="/css/main_page.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script defer src="/scripts/auth.js" />
         <title>{title}</title>
