8ec73db7536804dbdc7bfec3ed16e53a1a63f6b5
icy1xtee <easton2904@mail.ru>
2024-02-08 15:48:32 +0300

    scripts problem fixed

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,11 +6,11 @@ module.exports = function Layout({ title, children }) {
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="/css/style-reset.css" />
         <link rel="stylesheet" href="/css/styles.css" />
         <link rel="stylesheet" href="/css/navbar.css" />
         <link rel="stylesheet" href="/css/main_page.css" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script defer src="/scripts/auth.js" />
         <script defer src="/scripts/filter.js" />
         <title>{title}</title>
